12122190c3531314930a0921d17e738545bdae50
anzhiqing <anzhiqing@panda.tv>
2019-01-22 18:41:11 +0800

    Revert "Revert "1""
    This reverts commit 3b617d248d95b81f45d55934e314e9961ddd6ea6.

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const package = require('./package.json');
+const chalk = require('chalk');
+
+const { build } = require('./bin');
+
+process.title = 'zac';
+
+program
+  .command('build')
+  .description('build your page, build [page]')
+  .alias('b')
+  .action((page) => {
+    build({
+      page,
+    })
+  });
+program.parse(process.argv);

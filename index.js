9536fe24e5d29b921e87a97096c0adb5b8853c78
aio <zhiqing.an@kuwo.cn>
2019-04-19 14:59:00 +0800

    log

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
 const config = require('./config/index');
 const pkg = require('./package.json');
 
-const xlog = require('./util/xlog');
-const {
-  requireIfExsit,
-  requireMiddleware,
-} = require('./util/index');
+const xlog = require('./util/log');
+// const {
+//   requireIfExsit,
+//   requireMiddleware,
+// } = require('./util/index');
 
 module.exports = function (options) {
-  xlog.info(c => c.green('********start********'));
-
-  requireMiddleware('before-run', { pkg, options });
-
-  xlog.info('Using muse', c => c.magenta(config('ENV.root')));
-  xlog.info('Muse version', c => c.magenta(config('ENV.version')));
-  xlog.info('Current directory', c => c.magenta(config('ENV.cwd')));
-
-  requireMiddleware('before-task', config());
-
-  process.chdir(config('PROJECT.working_path'));
-
-  xlog.info('Change working path to', c => c.magenta(config('PROJECT.working_path')));
-
-  const processor = requireIfExsit(__dirname, `processor/${config('OPTIONS.action')}`);
-  if (!processor) {
-    xlog.error(`${config('OPTIONS.action')} processor not found! `);
-    process.exit(-1);
-  }
-
-  processor().on('close', (CODE) => {
-    requireMiddleware('after-task', Object.assign(config(), {
-      CODE,
-    }));
-  });
+  xlog.code(
+    {
+      content: [
+        '欢迎使用QING构建您的项目',
+      ]
+    }
+  )
+  xlog.info('Using Node ', c => c.magenta(options.argv[0]));
+  xlog.info('Using ' + pkg.name, c => c.magenta(options.argv[1]));
+  xlog.info('This version', c => c.magenta(pkg.version));
+  xlog.info('Current directory', c => c.magenta(process.cwd()));
 };

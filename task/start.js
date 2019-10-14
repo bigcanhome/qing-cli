0c1ebda8056a54f1ba65dd9580fbbdb002576829
uaio <819284657@qq.com>
2019-10-14 21:28:52 +0800

    fixbug

--- task/start.js
+++ task/start.js
@@ -1,40 +1,40 @@
-const { merge } = require('lodash');
-const path = require('path');
+const { merge } = require("lodash");
+const path = require("path");
 
-const before = require('./before');
-const { requireConfig } = require('../utils');
-const log = require('../utils/log');
+const before = require("./before");
+const { requireConfig } = require("../utils");
+const log = require("../utils/log");
 
 module.exports = function(options) {
-  if (options.params.name === 'none') {
+  if (options.params.name === "none") {
     options.page = options.root;
   } else {
     options.page = path.resolve(options.root + `/${options.params.name}`);
   }
   process.chdir(options.page);
-  log.info('Change working path to', c => c.magenta(options.page));
+  log.info("Change working path to", c => c.magenta(options.page));
 
   const cfgArr = [];
   if (options.params.isCurrDir) {
-    cfgArr.push([options.page, '../qing.config.js']);
+    cfgArr.push([options.page, "../qing.config.js"]);
   } else {
-    cfgArr.push([options.root, './qing.config.js']);
+    cfgArr.push([options.root, "./qing.config.js"]);
   }
-  cfgArr.push([options.page, './qing.config.js']);
+  cfgArr.push([options.page, "./qing.config.js"]);
 
-  const defaultCfg = require('../config/default/qing.config');
+  const defaultCfg = require("../config/default/qing.config");
   const getFileData = requireConfig(cfgArr);
 
   if (getFileData.index === 0) {
     log.warning(
       ...[
-        '您没有设置 qing.config.js 使用了默认配置 幸运的是QING是开箱即用的',
-        '如要配置请参考: http://fe.pandatv.com/docs/muse/MUSERC.html'
+        "您没有设置 qing.config.js 使用了默认配置 幸运的是QING是开箱即用的",
+        "如要配置请参考: http://fe.pandatv.com/docs/muse/MUSERC.html"
       ]
     );
   }
   const cfg = merge(defaultCfg, getFileData.data);
   options.cfg = cfg;
-  log.success('QING配置读取成功！！');
+  log.success("QING配置读取成功！！");
   before(options);
 };
--- webpack/addon/happy-pack.js
+++ webpack/addon/happy-pack.js
@@ -1,30 +1,31 @@
-const os = require('os');
+const os = require("os");
 
-const { merge } = require('lodash');
-const HappyPack = require('happypack');
+const { merge } = require("lodash");
+const HappyPack = require("happypack");
 
 const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
 
-const { requireConfig } = require('../../utils');
-const log = require('../../utils/log');
+const { requireConfig } = require("../../utils");
+const log = require("../../utils/log");
 
 module.exports = function(webpackConf, config) {
   const { page } = config;
   const cfgArr = [];
-  cfgArr.push([page, './babel.config.js']);
-  const defaultCfg = require('../../config/default/qing.config');
+  cfgArr.push([page, "./babel.config.js"]);
+  const defaultCfg = require("../../config/default/babel.config")(config);
   const cfg = merge(defaultCfg, requireConfig(cfgArr).data);
-  log.success('BABEL配置读取成功！！');
+
+  log.success("BABEL配置读取成功！！");
   webpackConf.plugins.push(
     new HappyPack({
-      id: 'babel',
+      id: "babel",
       loaders: [
         {
-          loader: require.resolve('babel-loader'),
+          loader: require.resolve("babel-loader"),
           options: merge(cfg, {
             babelrc: false,
             configFile: false,
-            sourceType: 'unambiguous'
+            sourceType: "unambiguous"
           })
         }
       ],
